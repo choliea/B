af3c33ff653d4a1f905c954e813e8d78b35c18e6
yukang <choliea91@gmail.com>
2023-10-11 01:11:11 +0900

    ds

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		// 모든 경로에 대해
 		registry.addMapping("/**")
 				// Origin이 http:localhost:3000에 대해
-				.allowedOrigins("http://localhost:3000","http://yk-todo-ui-service.ap-northeast-2.elasticbeanstalk.com/","https://yk-todo-ui-service.ap-northeast-2.elasticbeanstalk.com/")
+				.allowedOrigins("http://localhost:3000","http://todo-app-service.ap-northeast-2.elasticbeanstalk.com/","https://todo-app-service.ap-northeast-2.elasticbeanstalk.com/")
 				// GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
